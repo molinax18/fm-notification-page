6cc88443331c0787a01cc243294dd39885c7539e
molinax18 <arielmolina8953@gmail.com>
2025-12-05 16:16:01 -0300

    feat: enhance Notification component layout
    feat: unread count display into Notification component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Notification from './components/notification/Notification';
 
 export default function App() {
   return (
-    <main className="bg-navy-50 min-h-dvh">
+    <main className="bg-navy-50 h-dvh sm:flex sm:justify-center sm:py-7">
       <Notification />
     </main>
   );
--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -4,21 +4,28 @@ import NotificationCard from './NotificationCard';
 
 export default function Notification() {
   const { state } = useNotificationContext();
+  const getNotificationUnreadAmount = () => {
+    return state.reduce((acc, n) => (!n.read ? acc + 1 : acc), 0);
+  };
 
   return (
-    <section>
-      <header>
-        <div>
-          <h3>Notications</h3>
-          <span>3</span>
+    <section className="flex flex-col gap-y-6 bg-white h-full py-6 px-4 sm:h-auto sm:w-full sm:max-w-lg sm:rounded-lg sm:shadow-md sm:px-6">
+      <header className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <h3 className="font-semibold text-navy-950">Notifications</h3>
+          <span className="bg-blue-950 font-semibold text-white text-sm rounded-sm px-3 py-px">
+            {getNotificationUnreadAmount()}
+          </span>
         </div>
 
-        <button>Mark all as read</button>
+        <button className="text-sm outline-none tr-d-200 hover:cursor-pointer hover:text-blue-950 focus:text-blue-950">
+          Mark all as read
+        </button>
       </header>
 
       <div>
         {state.map((nc) => (
-          <NotificationCard data={nc} />
+          <NotificationCard key={nc.id} data={nc} />
         ))}
       </div>
     </section>
